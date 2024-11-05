90701e7d0b286a89d95cc242f87a157d0d886352
Silvana <silvana.munoz@ucb.edu.bo>
2024-11-05 08:47:36 -0400

    Correct backend

--- users-backend/src/main/java/com/springboot/backend/review/userapp/users_backend/controllers/UserController.java
+++ users-backend/src/main/java/com/springboot/backend/review/userapp/users_backend/controllers/UserController.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,7 +25,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 
 
 
-
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
